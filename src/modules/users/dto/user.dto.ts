80ac65b7b3c078ad65177157c72de2ec1ab64cc9
Nursultan Elshatuly <sultanelshatuly@gmail.com>
2026-01-29 09:30:57 +0500

    feat(prisma): change prisma-client to prisma-client-js, add public user selector

---
+++ src/modules/users/dto/user.dto.ts
@@ -0,0 +1,20 @@
+import { PlatformRole, Tariff } from "@prisma/client";
+
+export class UserResponseDto {
+  id: string;
+  email: string | null;
+
+  firstName: string | null;
+  lastName: string | null;
+  nickname: string | null;
+  image: string | null;
+
+  emailVerified: Date | null;
+  wasOnline: Date | null;
+
+  createdAt: Date;
+  updatedAt: Date;
+
+  currentTariff: Tariff;
+  platformRole: PlatformRole;
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,21 +1,37 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { Prisma, User } from 'prisma-generated/client';
+import { UserResponseDto } from './dto/user.dto';
+import { Prisma } from '@prisma/client';
+import { userPublicSelect } from './prisma/user.select';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async getAll() {
-    return this.prisma.user.findMany({});
+  async getAll(): Promise<UserResponseDto[]> {
+    return this.prisma.user.findMany({ select: userPublicSelect });
     // return [{ id: 1, name: 'John Doe' }];
   }
 
-  async getUserById(id: string): Promise<User> {
+  async getUserById(id: string): Promise<UserResponseDto> {
     try {
       return await this.prisma.user.findUniqueOrThrow({
         where: {
           id,
+        },
+        select: {
+          id: true,
+          email: true,
+          firstName: true,
+          lastName: true,
+          nickname: true,
+          createdAt: true,
+          image: true,
+          emailVerified: true,
+          wasOnline: true,
+          platformRole: true,
+          updatedAt: true,
+          currentTariff: true,
         },
       });
     } catch (e) {
